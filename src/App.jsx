66267f03801275fedf1351afde09e3c7f824c396
Yokesh <130788935+Yokeshbbca@users.noreply.github.com>
2024-12-05 22:14:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Home from "./components/Home"
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import Body from "./components/Body"
 import Appointment from "./components/Appoinment"
 
 
